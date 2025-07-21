31e2d20cae15e8033968fb1975561c7113422fb0
nikunjlad <nikunjrlad@gmail.com>
2025-07-20 19:39:23 -0700

    updating the repository

--- analysis.py
+++ analysis.py
@@ -10,14 +10,19 @@ import warnings
 warnings.filterwarnings("ignore")
 
 X = np.load("data/train.npy")
-y = np.load("data/labels.npy")
+y = np.load("data/train_labels.npy")
 
 print(X.shape)
 print(y.shape)
 
 
+def find_number_of_uniques(df, feature):
+    print("Number of unique {} values: {}".format(feature, df[feature].nunique()))
+
+
 def SMOTE_Analysis(k, o, u):
     try:
+        print("k: {}, over: {}, under: {}".format(k, o, u))
         model = DecisionTreeClassifier()
         over = SMOTE(sampling_strategy=o, k_neighbors=k, random_state=2)
         under = RandomUnderSampler(sampling_strategy=u)
@@ -28,18 +33,24 @@ def SMOTE_Analysis(k, o, u):
         scores = cross_val_score(model, Xn, yn, scoring='roc_auc', cv=cv, n_jobs=-1)
         score = np.mean(scores)
         print("k={}, over={}, under={}, Mean ROC AUC: {:.3f}".format(k, o, u, score))
-        return [k,o,u]
+        return [k, o, u]
     except Exception as e:
-        return ""
+        print(e)
+        return None
+
+
+def count_unique(d, columns):
+    for column in columns:
+        print("Number of Unique values in column {} is {}".format(column, str(len(d[column].unique()))))
 
 
 if __name__ == "__main__":
     k_values = [1, 2, 3, 4, 5, 6, 7]
-    over_fact = [0.1, 0.3, 0.4]
-    under_fact = [0.5, 0.6, 0.7, 0.8]
+    over_fact = [0.2, 0.4, 0.5]
+    under_fact = [0.5, 0.6, 0.7]
     vals = [[k, o, u] for k in k_values for o in over_fact for u in under_fact]
 
-    with mp.Pool(processes=50) as pool:
+    with mp.Pool(processes=10) as pool:
         results = pool.starmap(SMOTE_Analysis, vals)
 
     print(results)
--- vis.py
+++ vis.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 from sklearn.metrics import auc
+import plotly.graph_objects as go
 
 
 def plot_auc_roc(fpr, tpr):
@@ -16,4 +17,62 @@ def plot_auc_roc(fpr, tpr):
 
     fig.update_yaxes(scaleanchor="x", scaleratio=1)
     fig.update_xaxes(constrain='domain')
+    fig.show()
+
+
+def create_histogram(dfm, title):
+    fig = px.histogram(dfm, x="click")
+    fig.update_layout(title=title)
+    fig.show()
+
+
+def trends_graph(trend):
+    fig = go.Figure(go.Scatter(name=trend["name"],
+                               x=trend["x"],
+                               y=trend["y"],
+                               hovertemplate=trend["hovers"],
+                               marker_color=trend["marker_color"]
+                               ))
+
+    fig.update_layout(
+        title=trend["title"],
+        xaxis_tickformat=trend["xtick"],
+        xaxis_title=trend["xtitle"],
+        yaxis_title=trend["ytitle"]
+    )
+
+    fig.show()
+
+
+def grouped_bars(grouped):
+    fig = go.Figure(data=[
+        go.Bar(name=grouped["bar1_name"], x=grouped["x"], y=grouped["y1"],
+               hovertemplate=grouped["hover"], marker_color=grouped["color1"]),
+        go.Bar(name=grouped["bar2_name"], x=grouped["x"], y=grouped["y2"],
+               hovertemplate=grouped["hover"], marker_color=grouped["color2"])
+    ])
+
+    # Change the bar mode
+    fig.update_layout(
+        title=grouped["title"],
+        xaxis_title=grouped["xtitle"],
+        yaxis_title=grouped["ytitle"],
+        barmode='group',
+        xaxis_type=grouped["xaxis_type"]
+    )
+    fig.show()
+
+
+def single_bars(df, single):
+    fig = px.bar(df, x=single["x"], y=single["y"],
+                 labels=single["labels"],
+                 color=single["color"],
+                 height=400)
+
+    fig.update_layout(
+        title=single["title"],
+        xaxis_title=single["xtitle"],
+        yaxis_title=single["ytitle"],
+        xaxis_type=single["xaxis_type"]
+    )
     fig.show()
